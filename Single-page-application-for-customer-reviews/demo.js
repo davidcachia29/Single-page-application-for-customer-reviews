53a9de938d0d6bc018d1abab650bbdf2a5827da1
davidcachia29 <davidcachia29@gmail.com>
2021-09-30 08:59:07 +0200

    Created pages, with routing

---
+++ Single-page-application-for-customer-reviews/demo.js
@@ -0,0 +1,28 @@
+﻿//Creating the route using injection 
+var MyApp = angular.module("MyApp", ['ngRoute']);
+
+
+//Creating route using ngRoute Module with service routeProvider
+MyApp.config(['$routeProvider',
+    function ($routeProvider) {
+        $routeProvider.
+            when('/Add', {
+                templateUrl: 'Views/add.html',
+                controller: 'AddController'
+            }).
+            when('/Edit', {
+                templateUrl: 'Views/edit.html',
+                controller: 'EditController'
+            }).
+            when('/Delete', {
+                templateUrl: 'Views/delete.html',
+                controller: 'DeleteController'
+            }).
+            when('/Home', {
+                templateUrl: 'Views/home.html',
+                controller: 'HomeController'
+            }).
+            otherwise({
+                redirectTo: '/Home'
+            });
+    }])
